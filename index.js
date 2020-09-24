aa0590bc40bf225846df32274e9f885f5b4102f5
alexrjj <alexsoares2517@gmail.com>
2020-09-24 13:08:24 -0300

    Connecting to database

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const moment = require('moment');
+const connection = require('./database/connection');
+
+// DB Connection
+connection.authenticate().then(() => {
+    console.log('DB Ok.');
+}).catch((err) => {
+    console.log(err);
+});
 
 // BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
